737caf04637cf7bd7fb2d5ea6e943a564b9088f1
Georg von Zengen <vonzeng@ibr.cs.tu-bs.de>
2011-04-15 16:43:14 +0200

    defined bundel

--- core/net/dtn/bundle.h
+++ core/net/dtn/bundle.h
@@ -1,14 +1,16 @@
 struct prim_block_t{
 	char offset_tab[16];
 	uint8_t *block;	
+	uint8_t size;
 };
 
 struct payload_block_t{
 	char offset_tab[4];
 	uint8_t *block;
+	uint8_t size;
 };
 
 struct bundle_t{
-	struct prim_block_t prim_block;	
-	struct payload_block_t payload_block;
+	struct prim_block_t prim;	
+	struct payload_block_t payload;
 };
