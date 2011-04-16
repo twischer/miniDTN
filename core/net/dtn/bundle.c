cdac492d8c5d08530780fa7fb6b09dac4e420a3a
Georg von Zengen <vonzeng@ibr.cs.tu-bs.de>
2011-04-16 16:34:34 +0200

    added bundle  set_attr

--- core/net/dtn/bundle.c
+++ core/net/dtn/bundle.c
@@ -1,36 +1,53 @@
 #include "net/dtn/bundle.h"
 #include "net/dtn/sdnv.h"
 
+/** 
+* brief creates a new bundle and allocates the minimum needed memory
+*/
 uint8_t *create_bundle(void)
 {
 	struct bundle_t bundle;
-	bundle.prim.offset_tab[0]=0;
-	bundle.prim.offset_tab[1]=1;
-	bundle.prim.block = (uint8_t *) malloc(1);
-	*bundle.prim.block = 0;
-	bundle.prim.size=1;
-
-	bundle.payload.offset_tab[0]=0;
-	bundle.payload.offset_tab[1]=1;
-	bundle.payload.block = (uint8_t *) malloc(1);
-	*bundle.payload.block = 1;
-	bundle.payload.size=1;
-
+	bundle.offset_tab[VERSION]=0;
+	bundle.offset_tab[FLAGS]=1;
+	bundle.block = (uint8_t *) malloc(1);
+	*bundle.block = 0;
+	bundle.size=1;
+	uint8_t i;
+	for (i=1; i<sizeof(bundle.offset_tab); i++){
+		bundle.offset_tab[i]=1
+	}
+	set_type(i);
 	return &bundle;
 }
 
-uint8_t set_prim_attr(uint8_t *bundle, uint8_t attr, uint64_t *val)
+void set_type(uint8_t val)
+{
+	memmove(bundle.block+bundle.offset_tab[TYPE]+1,bundle.block+bundle.offset_tab[TYPE],bundle.size-bundle.offset_tab[TYPE]);
+	memset(bundle.block+bundle.offset_tab[TYPE],1,1);
+	uint8_t i;
+	for(i=TYPE+1;i<sizeof(bundle.offset_tab);i++){
+		bundle.offset_tab[i]+=1;
+	}
+	bundle.size+=1;
+}
+
+/**
+*brief converts an integer value in sdnv and copies this to the right place in bundel
+*/
+uint8_t set_attr(uint8_t *bundle, uint8_t attr, uint64_t *val)
 {
 	sdnv_t sdnv;
 	size_t len = sdnv_encoding_len(*val);
-	flags=(uint8_t *) malloc(len);
+	sdnv=(uint8_t *) malloc(len);
 	sdnv_encode(*val,sdnv,len);
-	bundle.prim.block=(uint8_t *)realloc(bundle.prim.block,len+bundle.prim.size);
-	memcpy(bundle.prim.block+bundle.prim.offset_tab[attr],sdnv,len);
+	bundle.block=(uint8_t *)realloc(bundle.block,len+bundle.size);
+	memmove(bundle.block+bundle.offset_tab[attr]+len,bundle.block+bundle.offset_tab[attr],bundle.size-bundle.offset_tab[attr]);
+	memcpy(bundle.block+bundle.offset_tab[attr],sdnv,len);
+	uint8_t i;
+	for(i=attr+1;i<sizeof(bundle.offset_tab);i++){
+		bundle.offset_tab[i]+=len;
+	}
+	bundle.size+=len;
 	free(sdnv);
 }
-
-
-
-
 
--- core/net/dtn/bundle.h
+++ core/net/dtn/bundle.h
@@ -1,16 +1,31 @@
-struct prim_block_t{
-	char offset_tab[16];
+
+//primary block defines
+#define VERSION 0
+#define FLAGS 1
+#define LENGTH 2
+#define DEST_NODE 3 
+#define DEST_SERV 4
+#define SRC_NODE 5 
+#define SRC_SERV 6
+#define REP_NODE 7
+#define REP_SERV 8
+#define CUST_NODE 9
+#define CUST_SERV 10 
+#define TIME_STAMP 11
+#define TIME_STAMP_SEQ_NR 12
+#define LIFE_TIME 13
+#define FRAG_OFFSET 14
+#define APP_DATA_LEN 15
+
+//payload block defines
+#define TYPE 16
+#define P_FLAGS 17 
+#define P_LENGTH 18
+#define PAYLOAD 19
+
+struct bundle_t{
+	char offset_tab[20];
 	uint8_t *block;	
 	uint8_t size;
 };
 
-struct payload_block_t{
-	char offset_tab[4];
-	uint8_t *block;
-	uint8_t size;
-};
-
-struct bundle_t{
-	struct prim_block_t prim;	
-	struct payload_block_t payload;
-};
