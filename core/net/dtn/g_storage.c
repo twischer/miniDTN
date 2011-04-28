949db25fedd109022c34ce859e2c8f3518412457
Georg von Zengen <vonzeng@ibr.cs.tu-bs.de>
2011-04-28 16:01:05 +0200

    storage function pointer, read still fails
    (was: storage function pointer, read still fails)

--- core/net/dtn/g_storage.c
+++ core/net/dtn/g_storage.c
@@ -1,10 +1,19 @@
 #include "contiki.h"
 #include "net/dtn/storage.h"
+#include "cfs/cfs.h"
 #include "net/dtn/g_storage.h"
 #include "net/dtn/bundle.h"
 #include "net/dtn/sdnv.h"
 #include <stdlib.h>
 #include <stdio.h>
+
+#define DEBUG 1
+#if DEBUG
+#include <stdio.h>
+#define PRINTF(...) printf(__VA_ARGS__)
+#else
+#define PRINTF(...)
+#endif
 
 struct file_list_entry_t file_list[BUNDLE_STORAGE_SIZE];
 char *filename = BUNDLE_STARAGE_FILE_NAME; 
@@ -24,16 +33,16 @@ void init(void){
 			file_list[i].lifetime=0;
 		}
 	}
+}
 
-
-int32_t save_bundle(uint8_t *offset_tab, struct bundle_t *bundle)
+int32_t save_bundle(struct bundle_t *bundle)
 {
 	uint16_t i=0;
 	int32_t free=-1;
 	uint8_t *tmp=bundle->block;
 	tmp=tmp+bundle->offset_tab[SRC_NODE][OFFSET];
 	uint32_t src;
-	sdnv_decode(tmp, , &src);
+	sdnv_decode(tmp ,bundle->offset_tab[SRC_NODE][STATE], &src);
 	tmp=bundle->block+bundle->offset_tab[TIME_STAMP][OFFSET];
 	uint32_t time_stamp;
 	sdnv_decode(tmp, bundle->offset_tab[TIME_STAMP][STATE], &time_stamp);
@@ -45,29 +54,35 @@ int32_t save_bundle(uint8_t *offset_tab, struct bundle_t *bundle)
 	sdnv_decode(tmp, bundle->offset_tab[FRAG_OFFSET][STATE], &fraq_offset);
 	
 	while ( i < BUNDLE_STORAGE_SIZE) {
-		if (free == -1){
-			file_list[i].file_size == 0
+		if (free == -1 && file_list[i].file_size == 0){
 			free=(int32_t)i;
+			PRINTF("%u is a free slot\n",i);
 		}
+		PRINTF("%lu == %lu\n",time_stamp_seq, file_list[i].time_stamp_seq);
 		if ( time_stamp_seq == file_list[i].time_stamp_seq && 
 		    time_stamp == file_list[i].time_stamp &&
 		    src == file_list[i].src &&
 		    fraq_offset == file_list[i].fraq_offset) {  // is bundle in storage?
+		    	PRINTF("%u is the same bundle\n",i);
 			return (int32_t)i;
-		}
+		}//else{
+		//	PRINTF("bundle are different\n");
+		//}
 		i++;
 	}
 	if(free == -1){
+		PRINTF("no free slots in bundlestorage\n");
 		return -1;
 	}
-	i--;
-	
+	i=(uint16_t)free;
+	PRINTF("bundle will be safed in solt %u\n",i);	
 	file_list[i].file_size = bundle->size+sizeof(bundle->offset_tab); 
 	tmp=bundle->block+bundle->offset_tab[LIFE_TIME][OFFSET];
 	sdnv_decode(tmp, bundle->offset_tab[LIFE_TIME][STATE], &file_list[i].lifetime);
 	
 	char b_file[5];
-	sprintf(b_file,"%u",file_list[i].file_num);
+	sprintf(b_file,"%u",file_list[i].bundle_num);
+	PRINTF("filename: %s\n", b_file);
 	fd_write = cfs_open(b_file, CFS_WRITE);
 	int n=0;
 	if(fd_write != -1) {
@@ -78,12 +93,16 @@ int32_t save_bundle(uint8_t *offset_tab, struct bundle_t *bundle)
 	}
 	fd_write = cfs_open(b_file, CFS_WRITE | CFS_APPEND);
 	if(fd_write != -1) {
-		n += cfs_write(fd_write, bundle->block, bundel->size);
+		n += cfs_write(fd_write, bundle->block, bundle->size);
 		cfs_close(fd_write);
 	}else{
 		return -1;
 	}
-	return (int32_t)file_list[i].file_num;
+	file_list[i].time_stamp_seq = time_stamp_seq;
+	file_list[i].time_stamp = time_stamp;
+	file_list[i].src = src ;
+	file_list[i].fraq_offset = fraq_offset;
+	return (int32_t)file_list[i].bundle_num;
 }
 
 uint16_t del_bundle(uint16_t bundle_num)
@@ -93,13 +112,15 @@ uint16_t del_bundle(uint16_t bundle_num)
 	cfs_remove(b_file);
 	file_list[bundle_num].file_size=0;
 	file_list[bundle_num].src=0;
+	return 1;
 }
 
-uint16_t read_bundle(uint16_t bundle_num,struct bundle_t bundle)
+uint16_t read_bundle(uint16_t bundle_num,struct bundle_t *bundle)
 {
-	char b_file[5],;
+	char b_file[5];
 	sprintf(b_file,"%u",bundle_num);
 	fd_read = cfs_open(b_file, CFS_READ);
+	
 	if(fd_read!=-1) {
 		cfs_read(fd_read, bundle->offset_tab, sizeof(bundle->offset_tab));
 		cfs_seek(fd_read, sizeof(bundle->offset_tab), CFS_SEEK_SET);
@@ -112,7 +133,7 @@ uint16_t read_bundle(uint16_t bundle_num,struct bundle_t bundle)
 
 
 }
-const struct storage_driver g_storage_driver = {
+const struct storage_driver g_storage = {
 	"G_STORAGE",
 	init,
 	save_bundle,
--- core/net/dtn/g_storage.h
+++ core/net/dtn/g_storage.h
@@ -5,6 +5,8 @@
 #define BUNDLE_STARAGE_FILE_NAME "list_file"
 #include "net/dtn/bundle.h"
 
+extern const struct storage_driver g_storage_driver;
+
 struct file_list_entry_t{
 	uint16_t bundle_num;
 	uint16_t file_size;
@@ -17,12 +19,11 @@ struct file_list_entry_t{
 extern struct file_list_entry_t file_list[BUNDLE_STORAGE_SIZE];
 
 
-
 void init(void);
 /**
 returns bundle_num
 */
-int32_t save_bundle(uint8_t *offset_tab, struct bundle_t *bundle);
+int32_t save_bundle(struct bundle_t *bundle);
 
 uint16_t del_bundle(uint16_t bundle_num);
 
