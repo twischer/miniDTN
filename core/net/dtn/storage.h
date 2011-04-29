3a8c99ca30371bfecbe798f08c8e8d62c89faf5d
Georg von Zengen <vonzeng@ibr.cs.tu-bs.de>
2011-04-29 15:30:46 +0200

    fixed storage read function
    (was: fixed storage read function)

--- core/net/dtn/storage.h
+++ core/net/dtn/storage.h
@@ -8,6 +8,7 @@ struct storage_driver {
 	char *name;
 
 	void (* init)(void);
+	void (* reinit)(void);
 	int32_t (* save_bundle)(struct bundle_t *bundle);
 	uint16_t (* del_bundle)(uint16_t bundle_num);
 	uint16_t (* read_bundle)(uint16_t bundle_num,struct bundle_t *bundle);
