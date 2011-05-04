15fc669f196defc18ab629813f43aca35c4a28a9
Georg von Zengen <vonzeng@ibr.cs.tu-bs.de>
2011-05-04 16:08:15 +0200

    started basic redundance check

---
+++ core/net/dtn/b_redundance.c
@@ -0,0 +1,13 @@
+
+static struct red_bundle_t *list;
+
+uint8_t check(struct bundle_t *bundle)
+{
+
+
+}
+uint8_t set(struct bundle_t *bundel)
+{
+	struct red_bundle_t *new_bundle= (struct *red_bundle_t) malloc sizeof(struct red_bundle_t);
+//TODO struct füllen und einfügen	
+}
