0d556a216b7108ba53d7b332f6aae9ffed186036
Georg von Zengen <vonzeng@ibr.cs.tu-bs.de>
2011-05-05 09:44:20 +0200

    b_redundance

--- core/net/dtn/b_redundance.c
+++ core/net/dtn/b_redundance.c
@@ -9,5 +9,9 @@ uint8_t check(struct bundle_t *bundle)
 uint8_t set(struct bundle_t *bundel)
 {
 	struct red_bundle_t *new_bundle= (struct *red_bundle_t) malloc sizeof(struct red_bundle_t);
+	sdnv_decode(bundel->block + bundle->offset_tab[SRC_NODE][OFFSET], sdnv_len(bundel->block + bundle->offset_tab[SRC_NODE][OFFSET]), &new_bundle->src);
+	sdnv_decode(bundel->block + bundle->offset_tab[TIME_STAMP_SEQ_NR][OFFSET], sdnv_len(bundel->block + bundle->offset_tab[TIME_STAMP_SEQ_NR][OFFSET]), &new_bundle->seq_nr);
+	sdnv_decode(bundel->block + bundle->offset_tab[FRAG_OFFSET][OFFSET], sdnv_len(bundel->block + bundle->offset_tab[FRAG_OFFSET][OFFSET]), &new_bundle->fraq_offset);
+	sdnv_decode(bundel->block + bundle->offset_tab[LIFE_TIME][OFFSET], sdnv_len(bundel->block + bundle->offset_tab[LIFE_TIME][OFFSET]), &new_bundle->lifetime);
 //TODO struct füllen und einfügen	
 }
