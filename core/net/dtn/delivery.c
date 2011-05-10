af11a37704987991396b9ada307adeb4b3fc239d
Georg von Zengen <vonzeng@ibr.cs.tu-bs.de>
2011-05-10 17:52:01 +0200

    some delivery work

--- core/net/dtn/delivery.c
+++ core/net/dtn/delivery.c
@@ -14,6 +14,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "net/dtn/custody-signal.h"
 
 #include "lib/list.h"
 #include "API/bundle.h"
@@ -47,22 +48,42 @@ void deliver_bundle(struct bundle_t *bundle, struct registration *n) {
 		if(n->status == APP_ACTIVE) {
 		
 
+			uint32_t len;
 			uint8_t *block = bundle->block + bundle->offset_tab[DATA][OFFSET];
 			while ( block <= bundle->block + bundle->offset_tab[DATA][OFFSET] + bundle->offset_tab[DATA][STATE]){
 				if (*block != 1){ // no payloadblock
-					uint32_t len;
 					uint8_t s_len= sdnv_len(bock+2);
 					sdnv_decode(block+2, s_len, &len);
 					block= block +2 + len +s_len;
 				}else{
 					uint8_t s_len= sdnv_len(bock+2);
+					sdnv_decode(block+2, s_len, &len);
 					process_post(n->application_process, submit_data_to_application_event, block +2 +s_len);
 					break;
 				}
 			}
 			block = bundle->block+1;
 			if (*block & 0x08){
-				CUSTODY.set_state(//TODO custody dings erzeugen
+				custody_signal_t cust_sig;	
+				cust_sig.status= CUSTODY_TRANSFER_SUCCEEDED;
+				uint8_t *tmp= bundle->block + bundle->offset_tab[DATA][OFFSET]+1; 
+				uint32_t flags;
+				sdnv_decode(bundle->block + bundle->offset_tab[FLAGS][OFFSET],bundle->offset_tab[FLAGS][STATE],&flags);
+				if (flags & 0x01){ //bundle is fragmented
+					sdnv_decode(bundle->block + bundle->offset_tab[FRAG_OFFSET][OFFSET], bundle->offset_tab[FRAG_OFFSET][STATE], &cust_sig.fragement_offset);
+					cust_sig.fragment_length=len;
+				}else{
+					cust_sig.fragment_length=0;
+					cust_sig.fragement_offset=0;
+				}
+				sndv_decode(bundle->block + bundle->offset_tab[TIME_STAMP][OFFSET], bundle->offset_tab[TIME_STAMP][STATE],  &cust_sig.bundle_creation_timestamp);
+				sndv_decode(bundle->block + bundle->offset_tab[TIME_STAMP_SEQ_NR][OFFSET],bundle->offset_tab[TIME_STAMP_SEQ_NR][STATE],  &cust_sig.bundle_creation_timestamp_seq);
+				sdnv_decode(bundle->block + bundle->offset_tab[SRC_NODE][OFFSET], bundle->offset_tab[SRC_NODE][STATE], &cust_sig.src_node);
+				sdnv_decode(bundle->block + bundle->offset_tab[SRC_SERV][OFFSET], bundle->offset_tab[SRC_SERV][STATE], &cust_sig.src_app);
+
+									
+				CUSTODY.set_state(&cust_sig);
+			}
 
 
 			/* Übergebe Nutzdaten jedes enthaltenen Payload Blocks an Anwendung */
