6180720d001dde3879ce91a0cdccbd0b8ab50c30
Georg von Zengen <vonzeng@ibr.cs.tu-bs.de>
2011-06-16 20:43:40 +0200

    hello-world: changed bundel->*mem  to bundle->mem
    (was: changed bundel->*mem  to bundle->mem)

--- examples/hello-world/hello-world.c
+++ examples/hello-world/hello-world.c
@@ -102,18 +102,22 @@ PROCESS_THREAD(hello_world_process, ev, data)
 			set_attr(&bundle, TIME_STAMP, &bla);
 			uint8_t *foo;
 			foo=(uint8_t *) malloc(10);
+			if (!foo){
+				printf("\n\n ERROR \n\n");
+				continue;
+			}
 			memset(foo,0xfe,10);
 			add_block(&bundle, 1,2,foo,10);
 			free(foo);
 			
 			printf("main size: %u\n",bundle.size);
-	//		uint8_t *tmp=bundle.block;
-	//		for(i=0; i<bundle.size; i++){
-	//			printf("%x ",*tmp);
-	//			tmp++;
+			uint8_t *tmp=(uint8_t *) bundle.mem.ptr;
+			for(i=0; i<bundle.size; i++){
+				printf("%x ",*tmp);
+				tmp++;
 
-	//		}
-	//		printf("\n");
+			}
+		printf("\n");
 			process_post(&agent_process,dtn_send_bundle_event,(void *) &bundle);
 			etimer_set(&timer, CLOCK_SECOND);
 
