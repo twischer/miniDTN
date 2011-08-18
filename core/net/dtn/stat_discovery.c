7812d273c9380016b53af4f3762dd33818848a6e
Georg von Zengen <vonzeng@ibr.cs.tu-bs.de>
2011-08-18 11:22:51 +0200

    final
    (was: final)

--- core/net/dtn/stat_discovery.c
+++ core/net/dtn/stat_discovery.c
@@ -36,7 +36,7 @@ void s_dis_send(uint16_t num)
 	dest.u8[0]=*((uint8_t*)bundle.mem.ptr+bundle.offset_tab[DEST_NODE][OFFSET]);
 //	printf(" dest node %u= %u\n",bundle.offset_tab[DEST_NODE][OFFSET],*((uint8_t*)bundle.mem.ptr+bundle.offset_tab[DEST_NODE][OFFSET]));
 	delete_bundle(&bundle);
-*/	dest.u8[0]=0x4;
+*/	dest.u8[0]=0x3;
 	dest.u8[1]=0;
 //	printf(" %u \n",BUNDLE_STORAGE.get_bundle_num());
 //	if( BUNDLE_STORAGE.get_bundle_num() ==40){
