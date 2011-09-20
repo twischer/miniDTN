b9b3ed2a1e562ae7e20d3c6432e319756e2ac180
Georg von Zengen <vonzeng@ibr.cs.tu-bs.de>
2011-09-20 10:54:31 +0200

    eval
    (was: eval)

--- core/net/dtn/stat_discovery.c
+++ core/net/dtn/stat_discovery.c
@@ -28,15 +28,15 @@ static rimeaddr_t dest;
 void s_dis_send(uint16_t num)
 {
 //	printf("Discovery: send\n");
-	if (num ==255)
-		return;
+//	if (num ==255)
+//		return;
 /*	struct bundle_t bundle;
 	BUNDLE_STORAGE.read_bundle(num,&bundle);
 
 	dest.u8[0]=*((uint8_t*)bundle.mem.ptr+bundle.offset_tab[DEST_NODE][OFFSET]);
 //	printf(" dest node %u= %u\n",bundle.offset_tab[DEST_NODE][OFFSET],*((uint8_t*)bundle.mem.ptr+bundle.offset_tab[DEST_NODE][OFFSET]));
 	delete_bundle(&bundle);
-*/	dest.u8[0]=0x3;
+*/	dest.u8[0]=0x1;
 	dest.u8[1]=0;
 //	printf(" %u \n",BUNDLE_STORAGE.get_bundle_num());
 //	if( BUNDLE_STORAGE.get_bundle_num() ==40){
