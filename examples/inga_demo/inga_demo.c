429c63e8ce784bc802f811cb7dbf284f03e53121
Georg von Zengen <vonzeng@ibr.cs.tu-bs.de>
2011-10-13 11:37:28 +0200

    inserted PROCESS_YIELD

--- examples/inga_demo/inga_demo.c
+++ examples/inga_demo/inga_demo.c
@@ -1,55 +1,15 @@
-/*
- * Copyright (c) 2006, Swedish Institute of Computer Science.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. Neither the name of the Institute nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- *
- * This file is part of the Contiki operating system.
- *
- * $Id: hello-world.c,v 1.1 2006/10/02 21:46:46 adamdunkels Exp $
- */
-
-/**
- * \file
- *         A very simple Contiki application showing how Contiki programs look
- * \author
- *         Adam Dunkels <adam@sics.se>
- */
-
-#include "contiki.h"
 
 /*
- * mainclass.c
+ * inga_demo.c
  *
  *  Created on: 13.10.2009
  *      Author: Kulau
 
- * main
+ * 
  *
  */
 	
+#include "contiki.h"
 #include "interfaces/acc-adxl345.h"             //tested
 #include "interfaces/flash-microSD.h"           //tested
 #include "interfaces/flash-at45db.h"            //tested
@@ -72,6 +32,7 @@
 PROCESS(hello_world_process, "Hello world process");
 AUTOSTART_PROCESSES(&hello_world_process);
 /*---------------------------------------------------------------------------*/
+static struct etimer timer;
 PROCESS_THREAD(hello_world_process, ev, data)
 {
   PROCESS_BEGIN();
@@ -117,8 +78,11 @@ PROCESS_THREAD(hello_world_process, ev, data)
 #endif
 
         adc_init(ADC_SINGLE_CONVERSION, ADC_REF_2560MV_INT);
+	etimer_set(&timer,  CLOCK_SECOND*0.05);
 
         while (1) {
+		PROCESS_YIELD();
+		etimer_set(&timer,  CLOCK_SECOND);
                 int16_t tmp;
                 /*############################################################*/
                 //ADXL345 serial Test
