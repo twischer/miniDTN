cc0a8885bba81c0fda90e3270e66a4b0b59c4abb
Georg von Zengen <vonzeng@ibr.cs.tu-bs.de>
2011-10-17 16:31:23 +0200

    added gyroscope interface

---
+++ platform/inga/dev/gyro-sensor.h
@@ -0,0 +1,13 @@
+#ifndef __GYRO_SENSOR_H__
+#define __GYRO_SENSOR_H__
+
+#include "lib/sensors.h"
+
+extern const struct sensors_sensor gyro_sensor;
+
+#define X_AS 0
+#define Y_AS 1
+#define Z_AS 2
+
+
+#endif /* __GYRO-SENSOR_H__ */
