b1be0ebdbab0572cd59a9d4969dbca803955f350
Georg von Zengen <vonzeng@ibr.cs.tu-bs.de>
2011-10-17 16:32:05 +0200

    added pressure sensor interface

---
+++ platform/inga/dev/pressure-sensor.h
@@ -0,0 +1,12 @@
+#ifndef __PRESS_SENSOR_H__
+#define __PRESS_SENSOR_H__
+
+#include "lib/sensors.h"
+
+extern const struct sensors_sensor  PRESS_sensor;
+
+#define  TEMP 0
+#define  PRESS 1
+
+
+#endif /* __PRESS-SENSOR_H__ */
