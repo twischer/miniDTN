bb3a272074a047358cf4b54e871e2cc154fc43c0
Daniel Willmann <daniel@totalueberwachung.de>
2012-02-06 18:04:24 +0100

    profiling/test: Fix typo in TEST marcos and make types explicit

--- core/sys/test.h
+++ core/sys/test.h
@@ -4,7 +4,7 @@
 #include <avr/pgmspace.h>
 #include <stdio.h>
 
-#define TEST_FAIL(value, scale, unit) printf_P(PSTR("TEST:FAIL:%li:%li:%s\n"), value, scale, unit)
-#define TEST_PASS(value, scale, unit) printf_p(PSTR("TEST:PASS:%li:%li:%s\n"), value, scale, unit)
+#define TEST_FAIL(value, scale, unit) printf_P(PSTR("TEST:FAIL:%li:%li:%s\n"), (uint32_t)value, (uint32_t)scale, unit)
+#define TEST_PASS(value, scale, unit) printf_P(PSTR("TEST:PASS:%li:%li:%s\n"), (uint32_t)value, (uint32_t)scale, unit)
 
 #endif /* __TEST_H__ */
