e65def52dc8a1fc120c24add9c1fdd0392665af6
Daniel Willmann <daniel@totalueberwachung.de>
2012-03-03 01:36:02 +0100

    mmem: Implement mmem_realloc to properly change an existing allocation

--- core/lib/mmem.h
+++ core/lib/mmem.h
@@ -86,6 +86,7 @@ struct mmem {
 int  mmem_alloc(struct mmem *m, unsigned int size);
 void mmem_free(struct mmem *);
 void mmem_init(void);
+int mmem_realloc(struct mmem *mem, unsigned int size);
 
 #endif /* __MMEM_H__ */
 
