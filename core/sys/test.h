059ead619f2ac291606e6b0ecaec5e95e4df3e46
Daniel Willmann <daniel@totalueberwachung.de>
2012-03-07 20:18:12 +0100

    test.h: Remove AVR dependent functions to make test.h arch independent

--- core/sys/test.h
+++ core/sys/test.h
@@ -1,11 +1,10 @@
 #ifndef __TEST_H__
 #define __TEST_H__
 
-#include <avr/pgmspace.h>
 #include <stdio.h>
 
-#define TEST_FAIL(reason) printf_P(PSTR("TEST:FAIL:%s\n"), (reason))
-#define TEST_PASS() printf_P(PSTR("TEST:PASS\n"))
-#define TEST_REPORT(desc, value, scale, unit) printf_P(PSTR("TEST:REPORT:%s:%li:%li:%s\n"), (desc), ((uint32_t)value), ((uint32_t)scale), (unit))
+#define TEST_FAIL(reason) printf("TEST:FAIL:%s\n", (reason))
+#define TEST_PASS() printf("TEST:PASS\n")
+#define TEST_REPORT(desc, value, scale, unit) printf("TEST:REPORT:%s:%li:%li:%s\n", (desc), ((uint32_t)value), ((uint32_t)scale), (unit))
 
 #endif /* __TEST_H__ */
