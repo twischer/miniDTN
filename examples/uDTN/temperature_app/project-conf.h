2c1e7ddc004dce77ee8480a077bdaa57719aca1f
Wolf-Bastian Poettner <poettner@ibr.cs.tu-bs.de>
2012-04-19 13:13:45 +0200

    Support for new statistics interface, optimal use of RAM for storage

--- examples/uDTN/temperature_app/project-conf.h
+++ examples/uDTN/temperature_app/project-conf.h
@@ -1,4 +1,7 @@
-// Disable RIMEs queuebuf
+#ifndef __PROJECT_CONF_H__
+#define __PROJECT_CONF_H__
+
+// Disable the CSMA retransmission buffers
 #undef QUEUEBUF_CONF_NUM
 #define QUEUEBUF_CONF_NUM 0
 
@@ -11,25 +14,30 @@
 #define MMEM_CONF_SIZE 4000
 
 // Set our storage elements
-#define BUNDLE_CONF_STORAGE_SIZE 100
+#undef BUNDLE_CONF_STORAGE_SIZE
+#define BUNDLE_CONF_STORAGE_SIZE 110
 
 // Enable statistics
 #define STATISTICS_CONF_ELEMENTS 4
 #define STATISTICS_CONF_PERIOD 3600
+#define STATISTICS_CONF_CONTACTS 16
 
 // Set the link layer compatibility to IBR-DTN
 #define IBR_COMP 1
 
 // Disable profiling to save memory
 #undef PROFILES_CONF_MAX
-#define PROFILES_CONF_MAX 1 
+#define PROFILES_CONF_MAX 0 
 
 #undef PROFILES_CONF_STACKSIZE
-#define PROFILES_CONF_STACKSIZE 10
+#define PROFILES_CONF_STACKSIZE 0
 
 // Set our channel
 #undef CHANNEL_802_15_4
 #define CHANNEL_802_15_4 22
 
 // Set our PAN ID
+#undef IEEE802154_CONF_PANID
 #define IEEE802154_CONF_PANID 0x780
+
+#endif /* __PROJECT_CONF_H__ */
