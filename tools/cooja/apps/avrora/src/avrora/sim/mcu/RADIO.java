af0b374e4a8f03a6820db0b27fd5604336826fc6
Fredrik Osterlind <fros@sics.se>
2012-05-24 14:55:50 +0200

    Revert "applied patch by david kopf, by email 2012-05-19 22:58"
    This reverts commit 5b95b3c74e062f0de4a35a465ae1e2663f399878.

--- tools/cooja/apps/avrora/src/avrora/sim/mcu/RADIO.java
+++ tools/cooja/apps/avrora/src/avrora/sim/mcu/RADIO.java
@@ -32,108 +32,123 @@
 
 package avrora.sim.mcu;
 
-import avrora.sim.InterruptTable;
-import avrora.sim.RWRegister;
+import avrora.sim.*;
+import avrora.sim.radio.Radio;
+import avrora.sim.radio.Medium;
 import avrora.sim.radio.ATmega128RFA1Radio;
+import avrora.sim.state.*;
+import cck.text.StringUtil;
+import cck.util.Arithmetic;
+import cck.util.Util;
 
 /**
  * Internal Radio interface. Used on the <code>ATMega128RFA1</code> platform for radio communication.
  *
- * @author David Kopf
+ * @author Daniel Lee, Simon Han, David Kopf
  */
 public class RADIO extends AtmelInternalDevice implements RADIODevice, InterruptTable.Notification {
 
-    private TRX_STATE_Reg TRX_STATE_reg;
-    private TRXPR_Reg TRXPR_reg;
+    private static TRX_STATE_Reg TRX_STATE_reg;
+//    final TRX_STATUS_Reg TRX_STATUS_reg;
+    private static TRXPR_Reg TRXPR_reg;
 
-    private ATmega128RFA1Radio connectedRadio;
-    private RADIODevice connectedDevice;
+    private static ATmega128RFA1Radio connectedRadio;
+    private static RADIODevice connectedDevice;
 
     public void connect(ATmega128RFA1Radio r, RADIODevice d) {
         connectedRadio = r;
+ //       connectedTransmitter = connectedRadio.getTransmitter();
         connectedDevice = d;
     }
-
+    
     public RADIO(AtmelMicrocontroller m) {
         super("radio", m);
         TRX_STATE_reg = new TRX_STATE_Reg();
-        TRXPR_reg = new TRXPR_Reg();
+  //      TRX_STATUS_reg = new TRX_STATUS_Reg();
+        TRXPR_reg = new TRXPR_Reg();    
         installIOReg("TRX_STATE", TRX_STATE_reg);
+ //       installIOReg("TRX_STATUS", TRX_STATUS_reg);
         installIOReg("TRXPR", TRXPR_reg);
+    
     }
 
     /**
+     * Post interrupts
+     */
+ /*
+    private void postRADIOInterrupt(int n) {
+                        devicePrinter.println("posting interrupt " + n);
+        interpreter.setPosted(interruptNum1, true);
+    //    SPSR_reg.setSPIF();
+    }
+
+    private void unpostRADIOInterrupt() {
+                            devicePrinter.println("unposting interrupt ");
+        interpreter.setPosted(interruptNum1, false);
+   //     SPSR_reg.clearSPIF();
+    }
+*/
+    /**
      * TRXPR register. Emulates the TRXRST and SLPTR pins of an external RF231 radio.
      */
-    private static final int TRX_STATUS   = 0x141;
-    //-- Radio states ---------------------------------------------------------
-    private static final byte STATE_BUSY_RX      = 0x01;
-    private static final byte STATE_BUSY_TX      = 0x02;
-    private static final byte STATE_RX_ON        = 0x06;
-    private static final byte STATE_TRX_OFF      = 0x08;
-    private static final byte STATE_PLL_ON       = 0x09;
-    private static final byte STATE_SLEEP        = 0x0F;
-    private static final byte STATE_BUSY_RX_AACK = 0x11;
-    private static final byte STATE_BUSY_TX_ARET = 0x12;
-    private static final byte STATE_RX_AACK_ON   = 0x16;
-    private static final byte STATE_TX_ARET_ON   = 0x19;
-  //private static final byte STATE_TRANSITION   = 0x1F;
+  //  class TRXPR_Reg implements ActiveRegister {
+
+      //-- Radio states ---------------------------------------------------------
+    public static final byte STATE_BUSY_RX      = 0x01;
+    public static final byte STATE_BUSY_TX      = 0x02;
+    public static final byte STATE_RX_ON        = 0x06; 
+    public static final byte STATE_TRX_OFF      = 0x08;
+    public static final byte STATE_PLL_ON       = 0x09;
+    public static final byte STATE_SLEEP        = 0x0F;
+    public static final byte STATE_BUSY_RX_AACK = 0x11;
+    public static final byte STATE_BUSY_TX_ARET = 0x12;
+    public static final byte STATE_RX_AACK_ON   = 0x16;
+    public static final byte STATE_TX_ARET_ON   = 0x19;
 
     protected class TRXPR_Reg extends RWRegister {
-        protected byte lastSLP = 0;
+        protected byte lastSLP = 0,lastRST = 0;
         public void write(byte val) {
  //         if (devicePrinter != null) devicePrinter.println("RADIO: wrote " + StringUtil.toMultirepString(val, 8) + " to TRXPR");
-            super.write(val);
-
-            //Get the radio status
-     //       byte rf231status = interpreter.getDataByte(0x141);
-         //   byte rf231status = interpreter.getDataByte(connectedRadio.TRX_STATUS);
-         //   byte rf231status = interpreter.getDataByte(TRX_STATUS);
-         //   if (rf231status != connectedRadio.rf231State) devicePrinter.println("RADIO:status conflict "+ rf231status + " " + connectedRadio.rf231State);
-            byte rf231status = connectedRadio.rf231State;
-            /* TODO: Figure out what the datasheed means by this!
-               "After initiating a state change
-                by a rising edge at Bit SLPTR in radio transceiver states TRX_OFF, RX_ON or
-                RX_AACK_ON, the radio transceiver remains in the new state as long as the pin is
-                logical high and returns to the preceding state with the falling edge."
-            */
-            // The SLPTR bit is only effective on a toggle
-            if ((val&0x02) != lastSLP) {
-                if (lastSLP == 0 ) {
-      //            devicePrinter.println("RADIO: SLP pin raised, state = " + TRX_STATE_reg.read() + " status = " + rf231status);
-                    switch (rf231status) {
+           super.write(val);  
+     //      devicePrinter.println("RADIO: SLP pin change, state, status "+ TRX_STATE_reg.read());           
+            if ((val&0x02) != lastSLP) {     //SLP pin toggle
+                if (lastSLP == 0 ) {         //Raise pin
+                //    devicePrinter.println("RADIO: SLP pin raised");
+                    switch (TRX_STATE_reg.read()) {
                        //if TRX_OFF, -> SLEEP
                         case STATE_TRX_OFF:
-                            connectedRadio.pinChangeSLP(val);
-                            break;
+                           connectedRadio.pinChangeSLP(val);
+                        break;
                         //if PLL_ON or TX_ARET_ON -> BUSY_TX
-                        //TRX_STATUS will have been set to the corresponding busy state
-                     //  case STATE_PLL_ON:
-                     //  case STATE_TX_ARET_ON:
-                        case STATE_BUSY_TX:
-                        case STATE_BUSY_TX_ARET:
-                            connectedRadio.pinChangeSLP(val);
-                            break;
+                        case STATE_PLL_ON:
+                        case STATE_TX_ARET_ON:
+                           connectedRadio.pinChangeSLP(val);
+                        break;
                      }
-                } else {
-      //            devicePrinter.println("RADIO: SLP pin lowered, state = " + TRX_STATE_reg.read() + " status = " + rf231status);
-                    switch (rf231status) {
+                } else {                   //Lower pin
+                //    devicePrinter.println("RADIO: SLP pin lowered");
+                    switch (TRX_STATE_reg.read()) {
                         //if SLEEP, ->TRX_OFF
                         case STATE_SLEEP:
                             connectedRadio.pinChangeSLP(val);
                             break;
                     }
+ /* After initiating a state change
+by a rising edge at Bit SLPTR in radio transceiver states TRX_OFF, RX_ON or
+RX_AACK_ON, the radio transceiver remains in the new state as long as the pin is
+logical high and returns to the preceding state with the falling edge.
+*/
                 }
                 lastSLP = (byte) (val&0x02);
             }
-            /* RST high resets all registers to default values and is cleared automatically.
-             * The microcontroller has to set SLPTR to the default value
-             */
+ /* RST high resets all registers to default values and is cleared automatically.
+  * The microcontroller has to set SLPTR to the default value
+  */
             if ((val&0x01) != 0) {
                 connectedRadio.reset();
                  //->TRX_OFF
                  //delay?
-                 super.write((byte)(val&0x02));
+                 super.write((byte)(val&0x02)); //which should be zero
             }
         }
     }
@@ -141,26 +156,30 @@ public class RADIO extends AtmelInternalDevice implements RADIODevice, Interrupt
      * TRX_STATE control register.
      */
     protected class TRX_STATE_Reg extends RWRegister {
+        protected byte lastCMD = 0;
         public void write(byte val) {
-            //Send the command field to the radio.
-            //The return value adds the readonly TRAC_STATUS bits for the actual register write
-            super.write(connectedRadio.newCommand((byte) (val & 0x1F)));
+            //Write the register only if the command has changed.
+            //The newCommand mnethod will change the readonly TRAC_STATUS bits in RAM,
+            //which will cause a recursive call to this routine that must be ignored.
+            val = (byte) (val & 0x1F);
+            
+            if (val != lastCMD) {
+                lastCMD = val;
+//              if (devicePrinter != null) devicePrinter.println("RADIO: wrote " + val + " to TRX_STATE");
+                super.write(val);
+                connectedRadio.newCommand(val);
+            }
         }
-        /*
-        public byte read() {
-            byte rf231state = super.read();
-            if (devicePrinter !=null) devicePrinter.println("RADIO: reading TRXSTATE " + rf231state);
-            return rf231state;
-        }
-        */
     }
 
 
     public void force(int inum) {
-        if (devicePrinter != null) devicePrinter.println("RADIO: force " + inum);
+    devicePrinter.println("RADIO: force " + inum);
+ //       SPSR_reg.setSPIF();
     }
     public void invoke(int inum) {
-        if (devicePrinter != null) devicePrinter.println("RADIO: invoke " + inum);
+        devicePrinter.println("RADIO: invoke " + inum);
+ //       unpostSPIInterrupt();
     }
 
 }
