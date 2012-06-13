4698a4dc4772204f1b712d35138131fc673b561a
Fredrik Osterlind <fros@sics.se>
2012-06-13 15:55:51 +0200

    added copyright info

--- tools/cooja/apps/avrora/src/se/sics/cooja/avrmote/interfaces/MicaZID.java
+++ tools/cooja/apps/avrora/src/se/sics/cooja/avrmote/interfaces/MicaZID.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2009, Swedish Institute of Computer Science.
+ * Copyright (c) 2012, Swedish Institute of Computer Science.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
