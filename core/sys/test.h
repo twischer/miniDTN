d702827044deeff391d3bbde476f2d4be6223923
Wolf-Bastian Poettner <poettner@ibr.cs.tu-bs.de>
2013-02-10 17:33:56 +0100

    Test Framework: Added (platform specific) function declaration to get a precise time stamp

--- core/sys/test.h
+++ core/sys/test.h
@@ -7,4 +7,6 @@
 #define TEST_PASS() printf("TEST:PASS\n")
 #define TEST_REPORT(desc, value, scale, unit) printf("TEST:REPORT:%s:%li:%li:%s\n", (desc), ((uint32_t)value), ((uint32_t)scale), (unit))
 
+uint32_t test_precise_timestamp(uint8_t * is_valid);
+
 #endif /* __TEST_H__ */
