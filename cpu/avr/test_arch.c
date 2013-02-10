e16587384fa748ddb63ddfb0d9cd60991f4aea2d
Wolf-Bastian Poettner <poettner@ibr.cs.tu-bs.de>
2013-02-10 17:34:38 +0100

    INGA: add test function for precise timing

---
+++ cpu/avr/test_arch.c
@@ -0,0 +1,33 @@
+#include "test.h"
+#include "watchdog.h"
+#include "clock.h"
+
+uint32_t test_precise_timestamp(uint8_t * is_valid) {
+	clock_time_t now;
+	unsigned short now_fine;
+	int cnt = 0;
+
+	do {
+		now_fine = clock_time();
+		now = clock_seconds();
+		cnt ++;
+
+		/* In our experiments, this function sometimes takes so long,
+		 * that the watchdog triggers a reboot
+		 */
+		if( cnt % 500 ) {
+			watchdog_periodic();
+		}
+	} while (now_fine != clock_time() && cnt < 65535);
+
+	if( is_valid != NULL ) {
+		*is_valid = 1;
+	}
+
+	/* It may happen, that the loop was aborted due to the counter */
+	if( cnt >= 65535 && is_valid != NULL ) {
+		*is_valid = 0;
+	}
+
+	return ((unsigned long)now)*CLOCK_SECOND + now_fine%CLOCK_SECOND;
+}
