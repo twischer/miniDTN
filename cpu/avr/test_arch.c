8d0b5fa97e6939f1b931672a1ded45fe69d15a06
Georg von Zengen <vonzeng@ibr.cs.tu-bs.de>
2013-03-12 16:12:51 +0100

    removed unnessesary calculations from precise time calculation for avr cpus

--- cpu/avr/test_arch.c
+++ cpu/avr/test_arch.c
@@ -10,24 +10,7 @@ uint32_t test_precise_timestamp(uint8_t * is_valid) {
 	do {
 		now_fine = clock_time();
 		now = clock_seconds();
-		cnt ++;
-
-		/* In our experiments, this function sometimes takes so long,
-		 * that the watchdog triggers a reboot
-		 */
-		if( cnt % 500 ) {
-			watchdog_periodic();
-		}
 	} while (now_fine != clock_time() && cnt < 65000);
-
-	if( is_valid != NULL ) {
-		*is_valid = 1;
-	}
-
-	/* It may happen, that the loop was aborted due to the counter */
-	if( cnt >= 65000 && is_valid != NULL ) {
-		*is_valid = 0;
-	}
 
 	return ((unsigned long)now)*CLOCK_SECOND + now_fine%CLOCK_SECOND;
 }
