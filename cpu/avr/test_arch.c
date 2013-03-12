dd755aae91357cf4d624e2cf17b85d967215e9ea
Wolf-Bastian Poettner <poettner@ibr.cs.tu-bs.de>
2013-03-12 16:26:54 +0100

    Test: changed signature of test_precise_timestamp to remove the pointer

--- cpu/avr/test_arch.c
+++ cpu/avr/test_arch.c
@@ -2,15 +2,14 @@
 #include "watchdog.h"
 #include "clock.h"
 
-uint32_t test_precise_timestamp(uint8_t * is_valid) {
+uint32_t test_precise_timestamp() {
 	clock_time_t now;
 	unsigned short now_fine;
-	uint16_t cnt = 0;
 
 	do {
 		now_fine = clock_time();
 		now = clock_seconds();
-	} while (now_fine != clock_time() && cnt < 65000);
+	} while (now_fine != clock_time());
 
 	return ((unsigned long)now)*CLOCK_SECOND + now_fine%CLOCK_SECOND;
 }
