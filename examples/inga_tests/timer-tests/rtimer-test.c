7ec0a8f4649e9e50183302df961b083f30a9b450
Enrico Joerns <e.joerns@tu-bs.de>
2013-08-08 01:49:13 +0200

    Adapted rtimer-test to test suite

--- examples/inga_tests/timer-tests/rtimer-test.c
+++ examples/inga_tests/timer-tests/rtimer-test.c
@@ -34,56 +34,63 @@
 
 #include <stdio.h> /* For printf() */
 #include "rtimer.h"
+#include "../test.h"
+
+TEST_SUITE("rtimer");
 /*---------------------------------------------------------------------------*/
 PROCESS(rtimer_test_process, "RTimer Test process");
 AUTOSTART_PROCESSES(&rtimer_test_process);
 /*---------------------------------------------------------------------------*/
-int RUNNING = 1;
+int running = 1;
 struct etimer et;
 struct rtimer rt;
-uint16_t cur_time, after_rtimer;
-int ret;	
-uint8_t DONE;
+static uint16_t cur_time, after_rtimer;
+static int ret;	
+static uint8_t done;
+
 void rtime_call(struct rtimer *t, void *ptr){
-	after_rtimer=RTIMER_NOW();
-	if (after_rtimer-cur_time ==  1000+1){
-		ret=0;
-	}else{
-		ret=1;
-	}
-	DONE=2;
+  after_rtimer=RTIMER_NOW();
+  if (after_rtimer - cur_time ==  1000 + 1){
+    ret = 0;
+  }else{
+    ret = 1;
+  }
+  done = 2;
 }
 /*---------------------------------------------------------------------------*/
 PROCESS_THREAD(rtimer_test_process, ev, data)
 {
-	PROCESS_BEGIN();
+  PROCESS_BEGIN();
 
-	etimer_set(&et, CLOCK_SECOND * 5);
-	PROCESS_YIELD_UNTIL(etimer_expired(&et));
+  TEST_BEGIN("rtimer-test");
 
-	while(RUNNING) {
-		if(DONE==0){
-			printf("Starting test...\n");
-				cur_time=RTIMER_NOW();
-				rtimer_set(&rt, cur_time + 1000, 0, rtime_call,NULL);
-		}
-		if(DONE==2){
-			printf("Test finished with %d\n", ret);
+  etimer_set(&et, CLOCK_SECOND * 5);
+  PROCESS_YIELD_UNTIL(etimer_expired(&et));
 
-			printf("Result: ");
-			if( ret == 0 ) {
-				printf("SUCCESS\n");
-				RUNNING = 0;
-			} else {
-				printf("FAILURE timediff = %d\n",after_rtimer-cur_time);
-				RUNNING = 0;
+  while(running) {
 
-			}
-		}
-		etimer_set(&et, CLOCK_SECOND * 1);
-		PROCESS_YIELD_UNTIL(etimer_expired(&et));
-	}
+    if(done == 0){
+      printf("Starting test...\n");
+      cur_time=RTIMER_NOW();
+      rtimer_set(&rt, cur_time + 1000, 0, rtime_call,NULL);
+    }
 
-	PROCESS_END();
+    if(done == 2){
+
+      running = 0;
+      TEST_REPORT("timediff = ", after_rtimer - cur_time, 1, "ms");
+
+      TEST_EQUALS(ret, 0);
+    }
+
+    etimer_set(&et, CLOCK_SECOND * 1);
+    PROCESS_YIELD_UNTIL(etimer_expired(&et));
+  }
+
+  TEST_END();
+
+  TESTS_DONE();
+
+  PROCESS_END();
 }
 /*---------------------------------------------------------------------------*/
