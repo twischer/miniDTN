b2f6d3c5f6097f539005cf16fcd49ee6763fb698
Enrico Joerns <e.joerns@tu-bs.de>
2013-11-06 21:16:51 +0100

    examples/inga: removed old files from demo dir

--- examples/inga/demo/button_demo.c
+++ examples/inga/demo/button_demo.c
@@ -8,7 +8,7 @@
  *	to set node_id, give NODE_ID=<x> with make
  *
  */
-	
+
 #include "contiki.h"
 #ifdef INGA_REVISION
 #include <avr/eeprom.h>
@@ -33,27 +33,27 @@ static struct etimer timer;
 PROCESS_THREAD(default_app_process, ev, data)
 {
   PROCESS_BEGIN();
-  	SENSORS_ACTIVATE(button_sensor);
-	
-	leds_init();	
-	leds_on(2);
-	leds_on(1);
-	etimer_set(&timer,  CLOCK_SECOND*0.05);
-        while (1) {
-			
-		PROCESS_YIELD();
-		if (ev == sensors_event && data == &button_sensor) {
-			leds_invert(1);
-		}
-		if ( etimer_expired(&timer)){
-			etimer_set(&timer,  CLOCK_SECOND);
-			leds_invert(2);
+  SENSORS_ACTIVATE(button_sensor);
 
-		}
-	
-        }
+  leds_init();	
+  leds_on(2);
+  leds_on(1);
+  etimer_set(&timer,  CLOCK_SECOND*0.05);
+  while (1) {
 
-  
+    PROCESS_YIELD();
+    if (ev == sensors_event && data == &button_sensor) {
+      leds_invert(1);
+    }
+    if ( etimer_expired(&timer)){
+      etimer_set(&timer,  CLOCK_SECOND);
+      leds_invert(2);
+
+    }
+
+  }
+
+
   PROCESS_END();
 }
 /*---------------------------------------------------------------------------*/
