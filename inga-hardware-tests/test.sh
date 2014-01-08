d8de64bef69a6273d932341a072f6869b884bed1
Georg von Zengen <vonzeng@ibr.cs.tu-bs.de>
2014-01-08 15:09:09 +0100

    added support to print ftdi id on dymo

--- inga-hardware-tests/test.sh
+++ inga-hardware-tests/test.sh
@@ -5,6 +5,9 @@ cd ../tools/inga/inga_tool/
 make
 cd -
 ../tools/inga/inga_tool/inga_tool  -s -d /dev/ttyUSB1 | grep "Serial:" >> report.log
+../tools/inga/inga_tool/inga_tool  -s -d /dev/ttyUSB1 | grep "Serial:" | cut -f 2 -d" " > tapeprint
+#lpr -o landscape -o cpi=9 -o lpi=4 -o position=top-right -PTape tapeprint
+rm tapeprint
 sed -r "s/\x1B\[([0-9]{0,2}(;[0-9]{1,2})?)?[m|K]//g" last.log >> report.log
 rm tmp.log
 rm last.log
