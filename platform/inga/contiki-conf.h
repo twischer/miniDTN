837d9044a9b6079e729fcc0e4bd3afec09acd754
Enrico Joerns <e.joerns@tu-bs.de>
2014-06-04 13:02:41 +0200

    [inga] Major restructuring and cleanup of inga core
    All inga-related configuration defines are now prefixed with INGA_CONF_
    
    Address generation and handling is fixed and mainly relies on the EUI64.
    Also auto-generation was improved.
    The meaning of the node id for simulation and debugging only was emphasized.
    
    All configuration options were documented.

--- platform/inga/contiki-conf.h
+++ platform/inga/contiki-conf.h
@@ -118,9 +118,6 @@
 #define PACKETBUF_CONF_HDR_SIZE   0            //RF230 combined driver/mac handles headers internally
 #endif /* RF230BB */
 
-/* Replace lower 2 bytes of MAC with node ID  */
-#define EUI64_BY_NODE_ID          1
-
 /* 211 bytes per queue buffer */
 #define QUEUEBUF_CONF_NUM         8
 /* 54 bytes per queue ref buffer */
@@ -202,13 +199,13 @@
 /* Make nullrdc wait for the proper ACK before proceeding */
 #define NULLRDC_CONF_802154_AUTOACK 1
 /* Let the RF230 radio driver generate fake acknowledgements to make nullrdc happy */
-#define RF320_CONF_INSERTACK 1
+#define RF320_CONF_INSERTACK      1
 /* Number of CSMA attempts 0-7. 802.15.4 2003 standard max is 5. */
-#define RF230_CONF_FRAME_RETRIES    5
+#define RF230_CONF_FRAME_RETRIES  5
 /* CCA theshold energy -91 to -61 dBm (default -77).
  * Set this smaller than the expected minimum rssi to avoid packet collisions */
 /* The Jackdaw menu 'm' command is helpful for determining the smallest ever received rssi */
-#define RF230_CONF_CCA_THRES    -85
+#define RF230_CONF_CCA_THRES      -85
 /* Default is one RAM buffer for received packets. More than one may benefit multiple TCP connections or ports */
 #define RF230_CONF_RX_BUFFERS     3
 
@@ -251,59 +248,26 @@
 
 #define UIP_CONF_TCP_SPLIT        1
 
-// RADIO_PAN_ID
-#ifndef RADIO_CONF_PAN_ID
-#define RADIO_PAN_ID	IEEE802154_PANID
-#else /* RADIO_CONF_PAN_ID */
-#define RADIO_PAN_ID	RADIO_CONF_PAN_ID
-#endif /* RADIO_CONF_PAN_ID */
-
-/* Assure NODE_ID is not set manually */
-#ifdef NODE_ID
-#undef NODE_ID
-#warning Use NODE_CONF_ID to define your NodeId
-#endif /* NODE_ID */
-
-// NODE_ID
-#ifndef NODE_CONF_ID
-#define NODE_ID	0
-#else /* NODE_CONF_ID */
-#define NODE_ID	NODE_CONF_ID
-#endif /* NODE_CONF_ID */
-
-#ifndef NODE_CONF_EUI64
-#define NODE_EUI64 0, 0, 0, 0, 0, 0, 0, 0
-#else
-#define NODE_EUI64  NODE_CONF_EUI64
-#endif
-
-// RADIO_CHANNEL
-#ifndef RADIO_CONF_CHANNEL
-#define RADIO_CHANNEL	26
-#else /* RADIO_CONF_CHANNEL */
-#define RADIO_CHANNEL	RADIO_CONF_CHANNEL
-#endif /* RADIO_CONF_CHANNEL */
-
-// RADIO_TX_POWER
-#ifndef RADIO_CONF_TX_POWER
-#define RADIO_TX_POWER	0
-#else /* RADIO_CONF_TX_POWER */
-#define RADIO_TX_POWER	RADIO_CONF_TX_POWER
-#endif /* RADIO_CONF_TX_POWER */
+#include "inga-conf.h"
 
 /* ************************************************************************** */
 /* RPL Settings                                                               */
 /* ************************************************************************** */
 #if UIP_CONF_IPV6_RPL
 
-/* Define MAX_*X_POWER to reduce tx power and ignore weak rx packets for testing a miniature multihop network.
+/* Define MAX_*X_POWER to reduce tx power and ignore weak rx packets 
+ * for testing a miniature multihop network.
  * Leave undefined for full power and sensitivity.
  * tx=0 (3dbm, default) to 15 (-17.2dbm)
- * RF230_CONF_AUTOACK sets the extended mode using the energy-detect register with rx=0 (-91dBm) to 84 (-7dBm)
- *   else the rssi register is used having range 0 (91dBm) to 28 (-10dBm)
- *   For simplicity RF230_MIN_RX_POWER is based on the energy-detect value and divided by 3 when autoack is not set.
- * On the RF230 a reduced rx power threshold will not prevent autoack if enabled and requested.
- * These numbers applied to both Raven and Jackdaw give a maximum communication distance of about 15 cm
+ * RF230_CONF_AUTOACK sets the extended mode using the energy-detect register 
+ * with rx=0 (-91dBm) to 84 (-7dBm)
+ * else the rssi register is used having range 0 (91dBm) to 28 (-10dBm)
+ * For simplicity RF230_MIN_RX_POWER is based on the energy-detect value 
+ * and divided by 3 when autoack is not set.
+ * On the RF230 a reduced rx power threshold will not prevent autoack 
+ * if enabled and requested.
+ * These numbers applied to both Raven and Jackdaw give a maximum 
+ * communication distance of about 15 cm
  * and a 10 meter range to a full-sensitivity RF230 sniffer.
 #define RF230_MAX_TX_POWER 15
 #define RF230_MIN_RX_POWER 30
@@ -328,7 +292,7 @@
 /* Logging adds 200 bytes to program size */
 #define LOG_CONF_ENABLED         1
 
-/** Contiki Core Interface (has no function here)*/
+/** Contiki Core Interface (has no function here) */
 #define CCIF
 /** Contiki Loadable Interface (has no function here) */
 #define CLIF
