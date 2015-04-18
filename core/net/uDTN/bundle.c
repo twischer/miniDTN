ecf030c9c70ef3f90db35c4a8d621ffaf9091912
Georg von Zengen <vonzeng@ibr.cs.tu-bs.de>
2015-04-18 17:32:38 +0200

    switch address byte order

--- core/net/uDTN/bundle.c
+++ core/net/uDTN/bundle.c
@@ -593,14 +593,14 @@ uint16_t bundle_delete_bundle(struct mmem *bundlemem)
 linkaddr_t convert_eid_to_rime(uint32_t eid) {
 	/* FIXME: Is this endian-specific? If so, we should fix it properly! */
 	linkaddr_t dest;
-	dest.u8[0] = (eid & 0x000000FF) >> 0;
-	dest.u8[1] = (eid & 0x0000FF00) >> 8;
+	dest.u8[1] = (eid & 0x000000FF) >> 0;
+	dest.u8[0] = (eid & 0x0000FF00) >> 8;
 	return dest;
 }
 
 uint32_t convert_rime_to_eid(linkaddr_t * dest) {
 	/* FIXME: Is this endian-specific? If so, we should fix it properly! */
 	uint32_t eid = 0;
-	eid = (dest->u8[0] & 0xFF) + ((dest->u8[1] & 0xFF) << 8);
+	eid = (dest->u8[1] & 0xFF) + ((dest->u8[0] & 0xFF) << 8);
 	return eid;
 }
