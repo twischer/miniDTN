d3a2c720d99acfb2419937a36e28ab62c1439170
Georg von Zengen <vonzeng@ibr.cs.tu-bs.de>
2015-04-24 10:03:27 +0200

    fiexd more cfs includes

--- examples/uDTN/serializer-test/uDTN-serializer-test.c
+++ examples/uDTN/serializer-test/uDTN-serializer-test.c
@@ -47,7 +47,7 @@
 #include "sys/profiling/profiling.h"
 #include "watchdog.h"
 #include "list.h"
-#include "cfs-coffee.h"
+#include "core/cfs/coffee/cfs-coffee.h"
 
 #include "net/uDTN/agent.h"
 #include "net/uDTN/bundle.h"
