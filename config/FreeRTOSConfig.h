20b69612e03d2b81cdf8176602e7fc5c09f6d4fe
Timo Wischer <twischer@freenet.de>
2015-05-18 14:14:11 +0200

    DIsable freeRTOS preemption, because contiki processes uses a cooparative execution (TODO enable preeamption again, if mutexes were used)

--- config/FreeRTOSConfig.h
+++ config/FreeRTOSConfig.h
@@ -90,7 +90,7 @@
 
 extern uint32_t SystemCoreClock;
 
-#define configUSE_PREEMPTION			1
+#define configUSE_PREEMPTION			0
 #define configUSE_IDLE_HOOK				1
 #define configUSE_TICK_HOOK				1
 #define configCPU_CLOCK_HZ				( SystemCoreClock )
