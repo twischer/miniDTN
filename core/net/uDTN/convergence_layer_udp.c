6f92b3d41337c8c595838aba00f121fdf094befb
Timo Wischer <t.wischer@tu-bs.de>
2015-09-04 16:20:14 +0200

    Format the udp-cl implemenattaion

--- core/net/uDTN/convergence_layer_udp.c
+++ core/net/uDTN/convergence_layer_udp.c
@@ -12,57 +12,46 @@
 
 #include "convergence_layer_udp.h"
 
+static struct netconn* discovery_conn = 0;
 
-static struct netconn *conn;
-static struct netbuf *buf;
-static struct ip_addr *addr;
-static unsigned short port;
-/*-----------------------------------------------------------------------------------*/
-static void convergence_layer_udp_thread(void *arg)
+static void convergence_layer_udp_discovery_thread(void *arg)
 {
-  err_t err, recv_err;
-  
-  LWIP_UNUSED_ARG(arg);
+	LWIP_UNUSED_ARG(arg);
 
-  conn = netconn_new(NETCONN_UDP);
-  if (conn == NULL)
-  {
-	  printf("netconn_new failed\n");
-	  vTaskDelete(NULL);
-  }
-  else
-  {
-	err = netconn_bind(conn, IP_ADDR_ANY, 1234);
-	if (err == ERR_OK)
-	{
-	  while (1)
-	  {
-		recv_err = netconn_recv(conn, &buf);
-      
-		if (recv_err == ERR_OK)
-		{
-		  addr = netbuf_fromaddr(buf);
-		  port = netbuf_fromport(buf);
-		  netconn_connect(conn, addr, port);
-		  buf->addr.addr = 0;
-		  netconn_send(conn,buf);
-		  netbuf_delete(buf);
+	discovery_conn = netconn_new(NETCONN_UDP);
+	if (discovery_conn == NULL) {
+		printf("netconn_new failed\n");
+		vTaskDelete(NULL);
+		return;
+	}
+
+	if (netconn_bind(discovery_conn, IP_ADDR_ANY, CL_UDP_DISCOVERY_PORT) != ERR_OK) {
+		printf("netconn_bind failed\n");
+		netconn_delete(discovery_conn);
+		vTaskDelete(NULL);
+		return;
+	}
+
+	while (1) {
+		static struct netbuf* buf = 0;
+		if (netconn_recv(discovery_conn, &buf) == ERR_OK) {
+			struct ip_addr* addr = netbuf_fromaddr(buf);
+			unsigned short port = netbuf_fromport(buf);
+			netconn_connect(discovery_conn, addr, port);
+			buf->addr.addr = 0;
+			netconn_send(discovery_conn,buf);
+			netbuf_delete(buf);
 		}
-	  }
 	}
-	else
-	{
-	  netconn_delete(conn);
-	}
-  }
 }
+
 
 
 bool convergence_layer_udp_init(void)
 {
-  if ( !xTaskCreate(convergence_layer_udp_thread, "UDP-CL process", configMINIMAL_STACK_SIZE+100, NULL, 1, NULL) ) {
-	  return false;
-  }
+	if ( !xTaskCreate(convergence_layer_udp_discovery_thread, "UDP-CL discovery", configMINIMAL_STACK_SIZE+100, NULL, 1, NULL) ) {
+		return false;
+	}
 
-  return true;
+	return true;
 }
--- core/net/uDTN/convergence_layer_udp.h
+++ core/net/uDTN/convergence_layer_udp.h
@@ -3,6 +3,10 @@
 
 #include <stdbool.h>
 
+#define CL_UDP_DISCOVERY_PORT	4551
+#define CL_UDP_BUNDLE_PORT		4556
+
+
 bool convergence_layer_udp_init(void);
 
 #endif // CONVERGENCE_LAYER_UDP
