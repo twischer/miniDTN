95488091da2e50309a070995b949d0d823ae58f0
Timo Wischer <t.wischer@tu-bs.de>
2015-11-30 12:16:34 +0100

    Disable LED outputs for energie consumption measurement

--- Inc/led.h
+++ Inc/led.h
@@ -17,29 +17,44 @@
 /* Includes ------------------------------------------------------------------*/
 #include "stm32f4xx_hal.h"
 
+#define ENABLE_LEDS 1
+
+#if (ENABLE_LEDS == 1)
+#define LED_PORT	GPIOD
 typedef enum {
 	LED_GREEN	= GPIO_PIN_12,
 	LED_ORANGE	= GPIO_PIN_13,
 	LED_RED		= GPIO_PIN_14,
 	LED_BLUE	= GPIO_PIN_15,
 } LED_Pin_t;
-
+#else
+/* if the LEDs where disabled use
+ * other free pins for debugging output
+ */
+#define LED_PORT	GPIOA
+typedef enum {
+	LED_GREEN	= GPIO_PIN_8,
+	LED_ORANGE	= GPIO_PIN_15,
+	LED_RED		= GPIO_PIN_3,
+	LED_BLUE	= GPIO_PIN_10,
+} LED_Pin_t;
+#endif
 
 static inline void LED_Off(const LED_Pin_t led_pin)
 {
-	GPIOD->BSRR = led_pin << 16;
+	LED_PORT->BSRR = led_pin << 16;
 }
 
 
 static inline void LED_On(const LED_Pin_t led_pin)
 {
-	GPIOD->BSRR = led_pin;
+	LED_PORT->BSRR = led_pin;
 }
 
 
 static inline void LED_Toggle(const LED_Pin_t led_pin)
 {
-	GPIOD->ODR ^= led_pin;
+	LED_PORT->ODR ^= led_pin;
 }
 
 #ifdef __cplusplus
--- Src/gpio.c
+++ Src/gpio.c
@@ -72,6 +72,13 @@ void MX_GPIO_Init(void)
   GPIO_InitStruct.Speed = GPIO_SPEED_LOW;
   HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
 
+  /*Configure GPIO pins : PA3 PA8 PA10 PA15 */
+  GPIO_InitStruct.Pin = GPIO_PIN_3|GPIO_PIN_8|GPIO_PIN_10|GPIO_PIN_15;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
   /*Configure GPIO pins : PB0 PB1 PB14 */
   GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_14;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
